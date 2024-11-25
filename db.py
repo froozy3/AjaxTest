bbbc5f5a71409ba48a1b75fb16f8d69044e33d42
Vlad <tsemkalo2006@gmail.com>
2024-11-25 14:25:14 +0200

    Updating logic

--- db.py
+++ db.py
@@ -1,16 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres"
+load_dotenv()
+
+DATABASE_URL = f"postgresql://{os.getenv('name')}:{os.getenv('password')}@{os.getenv('localhost')}:5432/{os.getenv('db_name')}"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 
 
-# try:
-#     with engine.connect() as connection:
-#         print("Підключення до бази даних успішне!")
-# except Exception as e:
-#     print(f"Помилка підключення: {e}")
+def is_success_connection():
+    try:
+        with engine.connect() as connection:
+            print("Підключення до бази даних успішне!")
+    except Exception as e:
+        print(f"Помилка підключення: {e}")
 
 
 class Base(DeclarativeBase):
@@ -22,7 +27,7 @@ class Status(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     status: Mapped[str] = mapped_column(unique=True)
-    version: Mapped[str] = mapped_column(nullable=False, default="1")
+    version: Mapped[float] = mapped_column(nullable=False, default=1.0)
 
     def __repr__(self):
         return f"Status DB: {self.id}\n {self.status}\n{self.version}"
@@ -40,4 +45,6 @@ def get_db():
     finally:
         db.close()
 
+
 # create_table()
+is_success_connection()
--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from db import get_db, Status
 
 app = FastAPI()
@@ -9,6 +9,11 @@ SessionLocal = get_db()
 
 @app.post("/setStatus")
 def create(status: str, session: SessionLocal = Depends(get_db)):
+    existing_status = session.query(Status).filter(Status.status == status).first()
+
+    if existing_status:
+        raise HTTPException(status_code=422, detail="Such a status already exists")
+
     status_db = Status(status=status)
     session.add(status_db)
     session.commit()
@@ -16,24 +21,45 @@ def create(status: str, session: SessionLocal = Depends(get_db)):
 
 
 @app.patch("/updateStatus")
-def update(id: int, version: str, session: SessionLocal = Depends(get_db)):
+def update(id: int, version: float, session: SessionLocal = Depends(get_db)):
     status_db = session.get(Status, id)
+    if not status_db:
+        raise HTTPException(status_code=404, detail=f"Status not found by id")
+
+    if version <= 0:
+        raise HTTPException(status_code=400, detail="Version must be positive float or greater than 0")
+
+    if version != (round(status_db.version + 0.1, 1)):
+        raise HTTPException(status_code=400, detail="Version can only increase by 0.1")
+
     status_db.version = version
     session.commit()
     return {"message": "status updated successfully!"}
 
 
 @app.put("/rewriteStatus")
-def rewrite(id: int, status: str, version: str, session: SessionLocal = Depends(get_db)):
+def rewrite(id: int, status: str, version: float, session: SessionLocal = Depends(get_db)):
     status_db = session.get(Status, id)
+    if not status_db:
+        raise HTTPException(status_code=404, detail=f"Status not found by id")
+
+    if version <= 0:
+        raise HTTPException(status_code=400, detail="Version must be positive float or greater than 0")
+
+    if version != (round(status_db.version + 1)):
+        raise HTTPException(status_code=400, detail="Version can only increase by 1")
+
     status_db.status, status_db.version = status, version
     session.commit()
-    return {"message": "status updated successfully!"}
+    return {"message": "status rewrote successfully!"}
 
 
-@app.delete("/updateStatus/{id}")
+@app.delete("/removeStatus/{id}")
 def delete(id: int, session: SessionLocal = Depends(get_db)):
     status_db = session.get(Status, id)
+
+    if not status_db:
+        raise HTTPException(status_code=404, detail=f"Status not found by id")
     session.delete(status_db)
     session.commit()
     return {"message": "status deleted successfully!"}
@@ -42,11 +68,30 @@ def delete(id: int, session: SessionLocal = Depends(get_db)):
 @app.get("/status/{id}")
 def get(id: int, session: SessionLocal = Depends(get_db)):
     status_db = session.get(Status, id)
+
+    if not status_db:
+        raise HTTPException(status_code=404, detail=f"Status not found by id")
+
     return {"message": status_db}
 
-#
-# @app.post('/rollbackStatusVersion')
-# def rollback()
+
+@app.post('/rollbackStatusVersion')
+def rollback(id: int, session: SessionLocal = Depends(get_db)):
+    status_db = session.get(Status, id)
+
+    if not status_db:
+        raise HTTPException(status_code=404, detail=f"Status not found by id")
+
+    if status_db.version > 1:
+
+        if round(status_db.version - 1, 1) < 1:
+            raise HTTPException(status_code=400, detail="Cannot rollback. Version cannot go below 1.")
+
+        status_db.version -= 1
+        session.commit()
+        return {"message": "status rolled back successfully!"}
+    raise HTTPException(status_code=400, detail="Ooops! Status has a version smaller than 1 or stored value is 1")
+
 
 if __name__ == "__main__":
     uvicorn.run(app=app)
